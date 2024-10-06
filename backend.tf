09f580a5b4868ada30ac0a142514d0d5a63cace0
Palaparthi-David <palaparthidavid2@gmail.com>
2024-10-06 16:15:34 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "my-s3-bucket-for-tf-state"
+    bucket = "terraformbucket45"
     key = "main"
     region = "us-east-1"
-    dynamodb_table = "my-dynamo-db-table-tf"
+    dynamodb_table = "terraform_table"
   }
 }
